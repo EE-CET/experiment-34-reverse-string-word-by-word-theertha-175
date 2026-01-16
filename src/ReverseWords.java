5b02bda346dd709a056fd1ce962729c6126b7024
theertha-175 <tve24el010@cet.ac.in>
2026-01-16 10:28:54 +0530

    Update ReverseWords.java

--- src/ReverseWords.java
+++ src/ReverseWords.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 public class ReverseWords {
-    
-        // TODO: Read the sentence line
-        // TODO: Split the sentence into words (using split method)
-        // TODO: Iterate backwards through the array of words
-        // TODO: Build and print the reversed sentence
-    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String[] words = s.split(" ");
+		StringBuilder result = new StringBuilder();
+		for (int i = words.length - 1; i >= 0; i--) {
+			result.append(words[i]);
+			if (i != 0){
+				result.append(" ");
+			}
+		}
+		System.out.print(result.toString());
+		sc.close();
+	}
 }
